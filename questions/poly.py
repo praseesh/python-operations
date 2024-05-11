8423de70e22b0aa4bc8714a886a066d8c71cd67c
Praseesh <praseeshprasee@gmail.com>
2024-05-12 02:40:19 +0530

    changes removed

--- questions/poly.py
+++ questions/poly.py
@@ -1,7 +1,7 @@
 class BankAccount:
     def __init__(self, account_number, balance=0):
         self.__account_number = account_number  # private attribute
-        self.__balance = balance
+        self.__balance = balance  # private attribute
 
     def deposit(self, amount):
         if amount > 0:
@@ -23,11 +23,13 @@ class BankAccount:
     def get_account_number(self):
         return self.__account_number
 
+# Creating an instance of BankAccount
 account = BankAccount("123456", 1000)
 
-print("Account Number:", account.get_account_number())  
-print("Current Balance:", account.get_balance()) 
+# Accessing private attributes and methods using public methods
+print("Account Number:", account.get_account_number())  # Output: Account Number: 123456
+print("Current Balance:", account.get_balance())  # Output: Current Balance: 1000
 
-account.deposit(500)  
-account.withdraw(200)  
-account.withdraw(2000)  
+account.deposit(500)  # Output: Deposited $500. New balance: $1500
+account.withdraw(200)  # Output: Withdrew $200. New balance: $1300
+account.withdraw(2000)  # Output: Invalid withdrawal amount.
