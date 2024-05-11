1d8d1e64784885d9b91b9acc21ef90600e6bb743
Praseesh <praseeshprasee@gmail.com>
2024-05-12 02:38:09 +0530

    commit

--- questions/poly.py
+++ questions/poly.py
@@ -26,7 +26,7 @@ class BankAccount:
 # Creating an instance of BankAccount
 account = BankAccount("123456", 1000)
 
-print("Account Number:", account.get_account_number())  # Output: Account Number: 123456
+print("Account Number:", account.get_account_number())  
 print("Current Balance:", account.get_balance()) 
 
 account.deposit(500)  
