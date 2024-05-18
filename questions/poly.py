9059183deae75acf2894cbf7dbfdaa5230cb5584
Praseesh <praseeshprasee@gmail.com>
2024-05-18 16:23:13 +0530

    updated

--- questions/poly.py
+++ questions/poly.py
@@ -3,6 +3,7 @@ class BankAccount:
         self.__account_number = account_number  
         self.__balance = balance  
 
+
     def deposit(self, amount):
         if amount > 0:
             self.__balance += amount
