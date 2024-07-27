9921b4347ad3f45b088fe8d67b07263f23d83a22
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-07-27 18:39:47 +0530

    Add class

--- Dictionary_kwargs.py
+++ Dictionary_kwargs.py
@@ -6,3 +6,21 @@ def capitalize(**my_dict):
     
 my_dict = capitalize(name = "alaN", aGe = 33, pLaCe = "kOcHi" )
 print(my_dict)
+
+# defining car class
+class Car():
+    # args receives unlimited no. of arguments as an array
+    def __init__(self, *args):
+        # access args index like array does
+        self.speed = args[0]
+        self.color = args[1]
+
+
+# creating objects of car class
+audi = Car(200, 'red')
+bmw = Car(250, 'black')
+mb = Car(190, 'white')
+
+# printing the color and speed of the cars
+print(audi.color)
+print(bmw.speed)
